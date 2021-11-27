17c61a5301537930ea385b194d4f86f31f5bb32e
Alexa <70447594+alexa-nov@users.noreply.github.com>
2021-11-27 00:06:17 -0800

    Refactored code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Devices from './Devices.js'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
  
     this.state = {
-      data: [],
-      loaded: false
+      items: [],
+      loaded: false,
+      offset: 0
     };
   }
  
@@ -16,34 +18,21 @@ class App extends React.Component {
   }
 
   getDevices() {
-    fetch('https://www.ifixit.com/api/2.0/wikis/CATEGORY?offset=0&limit=50')
+    const baseUrl = 'https://www.ifixit.com/api/2.0/wikis/CATEGORY';
+    const url = baseUrl + '?offset=' + this.state.offset + '&limit=13';
+    fetch(url)
       .then(response => response.json())
       .then(data => this.setState({items: data, loaded: true}));
   }
   
   render() {
-    const { items, loaded } = this.state;
-    if (!loaded) return <div>
-      <h1> Items loading... </h1> 
-    </div> ;
-
   return (
-    <div className='Device-panel'>
-    <div className='Device-grid'>
-      {items.map((item, index) => (
-        <div key = { index }>
-        <img 
-          className='Device-image'
-          src ={item.image.standard} 
-          alt=""
-        />
-        </div>
-      ))}
-    </div>
+    <div>
+    <Devices
+      stateVars={this.state}
+    />
     </div>
   )
 }
 }
 export default App;
-
-
