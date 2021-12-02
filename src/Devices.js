44f39c2b9343f75ed09a93b11b60f5430f6788c0
Alexa <70447594+alexa-nov@users.noreply.github.com>
2021-12-01 19:44:02 -0800

    Update Devices.js

--- src/Devices.js
+++ src/Devices.js
@@ -1,65 +1,43 @@
 import React from 'react';
-import './App.css';
-import Bag from './Bag';
-import Devices from './Devices.js'
-import { useState } from 'react';
+import './Devices.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      loaded: false,
-      offset: 0
-    };
-  }
- 
-  componentDidMount() {
-    this.getDevices();
-  }
-
-
-  getDevices() {
-    const baseUrl = 'https://www.ifixit.com/api/2.0/wikis/CATEGORY';
-    const url = baseUrl + '?offset=' + this.state.offset + '&limit=12';
-    fetch(url)
-      .then(response => response.json())
-      .then(data => this.setState({items: data, loaded: true}));
-  }
-
-  nextPage(){
-    this.setState({ offset: this.state.offset + 12 });
-    console.log(updatedOffset);
-    console.log(this.state.offset);
-    this.getDevices();
-  }
-
-  prevPage(){
-    let updatedOffset = this.state.offset - 12
-    if (updatedOffset < 0){
-      updatedOffset = 0;
-    }
-    this.setState({
-      offset: updatedOffset
-    });
-    console.log(this.state.offset);
-    this.getDevices();
-  }
+class Devices extends React.Component {
   
   render() {
-  return (
-    <div>
-
-    <Devices
-      stateVars={this.state}
-      prev={() => this.prevPage()}
-      next={() => this.nextPage()}
-      />
-    <Bag
-      
-    />
+    if (!this.props.stateVars.loaded) return <div>
+      <h1> Items loading... </h1> 
+    </div> ;
+    return(
+    <div className='Device-panel'>
+    <div className='Device-grid'>
+    
+      {this.props.stateVars.items.map((item, index) => (
+        <div key = { index }>
+        <div className='Device-container'>
+        <img className='Device-image'
+          src ={item.image.standard} 
+          alt=""
+        />
+        </div>
+        <div className='Device-text'>
+          {item.display_title}
+        </div>
+        </div>
+      ))}
     </div>
-  )
+      <button 
+      onClick = {this.props.prev}
+      disabled = {this.props.stateVars.offset === 0}> 
+      prev
+      </button>
+      <button 
+      onClick = {this.props.next}> 
+      next 
+      </button>
+    </div>
+  );
 }
 }
-export default App;
+export default Devices;
+
+
