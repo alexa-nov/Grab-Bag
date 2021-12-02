f137bc8a5ad62fdaec63960b5fece63631fe9504
Alexa <70447594+alexa-nov@users.noreply.github.com>
2021-12-01 19:42:47 -0800

    Add files via upload

---
+++ src/Bag.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Bag.css';
+
+class Bag extends React.Component {
+  render() {
+    return(
+      <div className = "Bag-panel">
+      </div>
+    );
+}
+}
+export default Bag;
+
+
