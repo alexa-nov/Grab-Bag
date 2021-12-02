d5a7e86bca845055d5dea8fab58184a2c1a632c4
Alexa <70447594+alexa-nov@users.noreply.github.com>
2021-12-01 19:43:15 -0800

    Update Devices.js

--- src/Devices.js
+++ src/Devices.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Bag from './Bag';
 import Devices from './Devices.js'
+import { useState } from 'react';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
- 
     this.state = {
       items: [],
       loaded: false,
@@ -17,19 +18,45 @@ class App extends React.Component {
     this.getDevices();
   }
 
+
   getDevices() {
     const baseUrl = 'https://www.ifixit.com/api/2.0/wikis/CATEGORY';
-    const url = baseUrl + '?offset=' + this.state.offset + '&limit=13';
+    const url = baseUrl + '?offset=' + this.state.offset + '&limit=12';
     fetch(url)
       .then(response => response.json())
       .then(data => this.setState({items: data, loaded: true}));
+  }
+
+  nextPage(){
+    this.setState({ offset: this.state.offset + 12 });
+    console.log(updatedOffset);
+    console.log(this.state.offset);
+    this.getDevices();
+  }
+
+  prevPage(){
+    let updatedOffset = this.state.offset - 12
+    if (updatedOffset < 0){
+      updatedOffset = 0;
+    }
+    this.setState({
+      offset: updatedOffset
+    });
+    console.log(this.state.offset);
+    this.getDevices();
   }
   
   render() {
   return (
     <div>
+
     <Devices
       stateVars={this.state}
+      prev={() => this.prevPage()}
+      next={() => this.nextPage()}
+      />
+    <Bag
+      
     />
     </div>
   )
