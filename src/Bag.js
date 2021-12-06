dc679a1a0a450ea385229a185bfc6cfbf7066bf1
Alexa <70447594+alexa-nov@users.noreply.github.com>
2021-12-05 22:12:10 -0800

    Added drag and drop capabilities to the bag panel

--- src/Bag.js
+++ src/Bag.js
@@ -2,13 +2,58 @@ import React from 'react';
 import './Bag.css';
 
 class Bag extends React.Component {
+  constructor(props) {
+    super(props);
+    this.text = "";
+    this.onDrop=this.onDrop.bind(this)
+  }
+
+  addDeviceToBag(image, text) {
+    this.props.stateVars.bagImages.push(image);
+    this.props.stateVars.bagText.push(text);
+    this.forceUpdate();
+    localStorage.setItem('bagImages', JSON.stringify(this.props.stateVars.bag));
+    localStorage.setItem('bagText', JSON.stringify(this.props.stateVars.bag));
+  }
+
+
+  onDrop(e) {
+    e.preventDefault();
+    const img = e.dataTransfer.getData("image");
+    const txt = e.dataTransfer.getData("text");
+    this.addDeviceToBag(img, txt);
+  }
+
+  onDragOver(e) {
+    e.preventDefault();
+  }
+
   render() {
     return(
-      <div className = "Bag-panel">
+      <div className = "bag" onDrop={this.onDrop} onDragOver={this.onDragOver}>
+        {this.props.stateVars.bagImages.map((item, index) => {
+        return (
+        <div>
+          <div key={index}>
+            <img
+              className='image-bag'
+              alt=""
+              src ={item} 
+            />
+            </div>
+            </div>
+        )})
+        }
+        {this.props.stateVars.bagText.map((item, index) => {
+        return(
+        <div className = 'bag-text-container'>
+          <div className='bag-text'>
+            {item}
+          </div>
+        </div>
+        )})
+        }
       </div>
-    );
-}
-}
+)}};
+
 export default Bag;
-
-
