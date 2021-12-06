f117c5bb910fd5b094f62b7fbaffc73da8385618
Alexa <70447594+alexa-nov@users.noreply.github.com>
2021-12-05 22:14:39 -0800

    Updated with drag and drop capabilites

--- src/Devices.js
+++ src/Devices.js
@@ -2,42 +2,31 @@ import React from 'react';
 import './Devices.css';
 
 class Devices extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.onDragStart=this.onDragStart.bind(this)
+  }
+
+  onDragStart(e) {
+    e.dataTransfer.setData("image", this.props.image);
+    e.dataTransfer.setData("text", this.props.deviceText);
+  }
+
   render() {
-    if (!this.props.stateVars.loaded) return <div>
-      <h1> Items loading... </h1> 
-    </div> ;
     return(
-    <div className='Device-panel'>
-    <div className='Device-grid'>
-    
-      {this.props.stateVars.items.map((item, index) => (
-        <div key = { index }>
-        <div className='Device-container'>
-        <img className='Device-image'
-          src ={item.image.standard} 
+      <div>
+      <div className = 'Device-container'>
+      <img className='Device-image'
+          src ={this.props.image} 
           alt=""
-        />
-        </div>
-        <div className='Device-text'>
-          {item.display_title}
-        </div>
-        </div>
-      ))}
-    </div>
-      <button 
-      onClick = {this.props.prev}
-      disabled = {this.props.stateVars.offset === 0}> 
-      prev
-      </button>
-      <button 
-      onClick = {this.props.next}> 
-      next 
-      </button>
-    </div>
-  );
-}
+          onDragStart={this.onDragStart}
+      />
+      </div>
+      <div className='Device-text'>
+          {this.props.deviceText}
+      </div>
+      </div>
+    )
+  }
 }
 export default Devices;
-
-
